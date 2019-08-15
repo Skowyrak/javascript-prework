c6e24498dd3e60d89ef5b899fd59cf36d78554ba
Kasia Skowyra <skowyra.kasia@gmail.com>
2019-08-15 15:10:25 +0200

    game

--- js/script.js
+++ js/script.js
@@ -1,7 +1,62 @@
-let playerMove = 'papier';
+/* REFERENCJE DO HTML-A */
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!')
+const paperBtn = document.querySelector("#paperBtn");
+const rockBtn = document.querySelector("#rockBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
 
-let computerMove = `kamień`;
+const win = document.querySelector("#win");
+const score = document.querySelector("#score");
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+/* NASŁUCHIWACZE */
+
+paperBtn.addEventListener("click", function() {
+  determinePlayerMove("papier");
+});
+
+rockBtn.addEventListener("click", function() {
+  determinePlayerMove("kamień");
+});
+
+scissorsBtn.addEventListener("click", function() {
+  determinePlayerMove("nożyczki");
+});
+
+/* ZMIENNE GLOBALNE */
+
+let playerScore = 0;
+let playerChoice;
+let computerScore = 0;
+let computerChoice;
+
+//ruch komputera
+function computerMove() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) return "papier";
+  else if (randomNumber === 1) return "nożyczki";
+  else return "kamień";
+}
+
+//ruch gracza
+function determinePlayerMove(playerMove){
+  computerChoice = computerMove();
+  playerChoice = playerMove;
+  checkWinner();
+  score.innerHTML = playerScore + ":" + computerScore;
+}
+
+//wynik gry
+function checkWinner() {
+  if (
+    (playerChoice === "kamień" && computerChoice === "nożyczki") ||
+    (playerChoice === "papier" && computerChoice === "kamień") ||
+    (playerChoice === "nożyczki" && computerChoice === "papier")
+  ) {
+    win.innerHTML = "Wygrał gracz";
+    playerScore++;
+  } else if (playerChoice === computerChoice) {
+    win.innerHTML = "Remis";
+  } else {
+    win.innerHTML = "Wygrał komputer";
+    computerScore++;
+  }
+}
